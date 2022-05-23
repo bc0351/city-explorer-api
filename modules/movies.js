8f04ccd6498766f74bd32f21007890bb4d4fe0eb
bc <benchoe@2six.org>
2022-05-23 11:57:09 -0400

    cicd

--- modules/movies.js
+++ modules/movies.js
@@ -1,6 +1,5 @@
 'use strict';
 require('dotenv').config('../.env');
-const fs = require('fs/promises');
 
 let cache = require('./cache.js');
 module.exports = getMovies;
@@ -12,9 +11,6 @@ const axios = require('axios');
 async function getMovies(query) {
   const key = `movies-${query}`;
   const url = `${process.env.MOVIE_3_API_BASE_URL}/search/movie?api_key=${process.env.MOVIE_3_API_KEY}&query=${query}&sort_by=popularity.desc&format=json`;
-  
-  console.log(key);
-  console.log(url);
 
   if(cache[key] && (Date.now() - cache[key].timestamp < CACHE_CYCLE)) { //Cache timer set to 2 weeks
     console.log('Cache hit');
@@ -41,7 +37,7 @@ class Movie {
     this.total_votes = movie.vote_count,
     this.poster_path = movie.poster_path,
     this.backdrop_path = movie.backdrop_path,
-    this.img_url = (movie.backdrop_path) ? `https://image.tmdb.org/t/p/w300${movie.backdrop_path}` : (movie.poster_path) ? `https://image.tmdb.org/t/p/w300${movie.poster_path}` : undefined,
+    this.img_url = `https://api.themoviedb.org/3/movie/${movie.id}/images?api_key=${process.env.MOVIE_3_API_KEY}&language=en-US`,
     this.popularity = movie.popularity,
     this.released_on = movie.release_date
   }
