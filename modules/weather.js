94145586f288d788370777f3b36ad570cc0a57ca
bc <benchoe@2six.org>
2022-05-23 04:01:37 -0400

    lab10

--- modules/weather.js
+++ modules/weather.js
@@ -7,14 +7,14 @@ const axios = require('axios');
 const baseUrl = process.env.WEATHER_API_BASE_URL;
 const apiKey = process.env.WEATHER_API_KEY;
 
-function getForecast(req, res, next) {
+async function getForecast(req, res, next) {
   let params = {
     key: apiKey,
     lat: req.query.lat || '40.730610',
     lon: req.query.lon || '-73.935242',
     units: 'I'
   }
-  axios.get(baseUrl, { params })
+  await axios.get(baseUrl, { params })
     .then(response => response.data.data.map(forecast => { return new Forecast(forecast) }))
     .then(filteredCities => res.status(200).send(filteredCities))
     .catch(error => next(error));
